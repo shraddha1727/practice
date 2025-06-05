e32ffbecd63f3cb33d2a7a9c742dc6c1e455f575
shraddha1727 <dubeyshraddha740@email.com>
2025-06-05 12:56:47 +0530

    practice

---
+++ codec/hello.js
@@ -0,0 +1,16 @@
+
+    
+    for( let i=0;i>=10;i++)
+        {
+            if (i%2==0)
+            {
+                console.log("number is even")
+
+            }
+            else 
+            {
+                console.log("number is odd")
+            }
+
+    }
+    
